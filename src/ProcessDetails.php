ea5069f8e4ad0eb28c910d1d2d5705d24fe5b48a
dima <explossive1@gmail.com>
2019-02-13 09:35:40 +0300

    add fork child

---
+++ src/ProcessDetails.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace primipilus\daemon;
+
+/**
+ * Class ProcessDetails
+ *
+ * @package primipilus\daemon
+ */
+class ProcessDetails
+{
+    /** @var int  */
+    protected $id;
+    /** @var int process id pid */
+    protected $pid;
+
+    /**
+     * The constructor
+     *
+     * @param int $id
+     * @param int $pid
+     */
+    public function __construct(int $id, int $pid)
+    {
+        $this->id = $id;
+        $this->pid = $pid;
+    }
+
+    /**
+     * Get the pid
+     *
+     * @return int
+     */
+    public function id()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the pid
+     *
+     * @return int
+     */
+    public function pid()
+    {
+        return $this->pid;
+    }
+}
